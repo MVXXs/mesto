360de4e715a232822d49db220a4613bff56034dd
MVXX <kaurvelitz@gmail.com>
2023-01-23 22:04:38 +0300

    Внесение правок в JS, HTML&CSS (исправление отступов)

--- scripts/index.js
+++ scripts/index.js
@@ -1,31 +1,27 @@
 let btnPopupOpen = document.querySelector(".profile__button-edit");
 let popupContainer = document.querySelector(".popup");
 let btnPopupClose = document.querySelector(".popup__closed")
+let TitleDefaultText = document.querySelector(".profile__title");
+let SubtitleDefaultText = document.querySelector(".profile__subtitle");
+let inputName = document.querySelector(".popup__input_type_name");
+let inputAbout = document.querySelector(".popup__input_type_about");
 
 btnPopupOpen.addEventListener("click", popupOpen);
 btnPopupClose.addEventListener("click", popupClose);
 
 function popupOpen() {
     popupContainer.classList.add("popup_opened");
+    inputName.value = TitleDefaultText.textContent;
+    inputAbout.value = SubtitleDefaultText.textContent;
 }
 
 function popupClose() {
     popupContainer.classList.remove("popup_opened");
 }
 
+let popupForm = document.querySelector(".popup__form");
 
-let TitleDefaultText = document.querySelector(".profile__title");
-let SubtitleDefaultText = document.querySelector(".profile__subtitle");
-let inputName = document.querySelector("#name");
-let inputAbout = document.querySelector("#about");
-
-inputName.value = TitleDefaultText.textContent;
-inputAbout.value = SubtitleDefaultText.textContent;
-
-
-let btnSaved = document.querySelector(".popup__saved");
-
-btnSaved.addEventListener('click', infoSave);
+popupForm.addEventListener('submit', infoSave);
 
 function infoSave(evt) {
     evt.preventDefault();
